a8264ab1fd3bbf9b02992ac30380e4db1e75e780
Justin Brault <31930527+MrBrault@users.noreply.github.com>
2023-11-17 20:41:53 -0800

    Unified test 1
    Testing code for unified template

---
+++ unified/portfolio.js
@@ -0,0 +1,537 @@
+// let anchor = ['<a href="../abizael001"><h4>Click here to see another portfolio</h4></a>', '<a href="../adriana012"><h4>Click here to see another portfolio</h4></a>', '<a href="../alexander175"><h4>Click here to see another portfolio</h4></a>', '<a href="../averie765"><h4>Click here to see another portfolio</h4></a>', '<a href="../averie765"><h4>Click here to see another portfolio</h4></a>', '<a href="../ayleen681"><h4>Click here to see another portfolio</h4></a>', '<a href="../briana009"><h4>Click here to see another portfolio</h4></a>', '<a href="../carlos460"><h4>Click here to see another portfolio</h4></a>', '<a href="../cassius001"><h4>Click here to see another portfolio</h4></a>', '<a href="../citlaly001"><h4>Click here to see another portfolio</h4></a>', '<a href="../daniel031"><h4>Click here to see another portfolio</h4></a>', '<a href="../daniela077"><h4>Click here to see another portfolio</h4></a>', '<a href="../david005"><h4>Click here to see another portfolio</h4></a>', '<a href="../david866"><h4>Click here to see another portfolio</h4></a>', '<a href="../elizabeth592"><h4>Click here to see another portfolio</h4></a>', '<a href="../ellie329"><h4>Click here to see another portfolio</h4></a>', '<a href="../exael446"><h4>Click here to see another portfolio</h4></a>', '<a href="../imajyn001"><h4>Click here to see another portfolio</h4></a>', '<a href="../isaac603"><h4>Click here to see another portfolio</h4></a>', '<a href="../jenifer002"><h4>Click here to see another portfolio</h4></a>', '<a href="../jessica132"><h4>Click here to see another portfolio</h4></a>', '<a href="../jesus019"><h4>Click here to see another portfolio</h4></a>', '<a href="../jose107"><h4>Click here to see another portfolio</h4></a>', '<a href="../julian941"><h4>Click here to see another portfolio</h4></a>', '<a href="../keano703"><h4>Click here to see another portfolio</h4></a>', '<a href="../keegan839"><h4>Click here to see another portfolio</h4></a>', '<a href="../keira524"><h4>Click here to see another portfolio</h4></a>', '<a href="../kevin247"><h4>Click here to see another portfolio</h4></a>', '<a href="../laiza286"><h4>Click here to see another portfolio</h4></a>', '<a href="../luis043"><h4>Click here to see another portfolio</h4></a>', '<a href="../madden003"><h4>Click here to see another portfolio</h4></a>', '<a href="../maely984"><h4>Click here to see another portfolio</h4></a>', '<a href="../marisol575"><h4>Click here to see another portfolio</h4></a>', '<a href="../michael260"><h4>Click here to see another portfolio</h4></a>', '<a href="../nancy402"><h4>Click here to see another portfolio</h4></a>', '<a href="../noe399"><h4>Click here to see another portfolio</h4></a>', '<a href="../odalis001"><h4>Click here to see another portfolio</h4></a>', '<a href="../olivia568"><h4>Click here to see another portfolio</h4></a>', '<a href="../oscar254"><h4>Click here to see another portfolio</h4></a>', '<a href="../oscar400"><h4>Click here to see another portfolio</h4></a>', '<a href="../paulina102"><h4>Click here to see another portfolio</h4></a>', '<a href="../ronny001"><h4>Click here to see another portfolio</h4></a>', '<a href="../rosa353"><h4>Click here to see another portfolio</h4></a>', '<a href="../ruben135"><h4>Click here to see another portfolio</h4></a>', '<a href="../taryn900"><h4>Click here to see another portfolio</h4></a>', '<a href="../tven590"><h4>Click here to see another portfolio</h4></a>', '<a href="../uriel783"><h4>Click here to see another portfolio</h4></a>', '<a href="../venezia719"><h4>Click here to see another portfolio</h4></a>', '<a href="../yahir710"><h4>Click here to see another portfolio</h4></a>', '<a href="../yasmine743"><h4>Click here to see another portfolio</h4></a>', '<a href="../yuki001"><h4>Click here to see another portfolio</h4></a>'];
+
+let abizael001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let adriana012 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let alexander175 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let averie765 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let averie765 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let ayleen681 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let briana009 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let carlos460 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let cassius001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let citlaly001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let daniel031 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let daniela077 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let david005 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let david866 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let elizabeth592 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let ellie329 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let evelynjocelyn001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let exael446 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let imajyn001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let isaac603 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let jessica132 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let jesus019 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let jose107 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let julian941 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let keano703 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let keegan839 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let keira524 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let kevin247 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let kevin632 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let laiza286 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let luis043 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let madden003 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let maely984 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let marisol575 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let michael260 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let nancy402 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let noe399 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let odalis001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let olivia568 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let oscar254 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let oscar400 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let paulina102 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let ronny001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let rosa353 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let ruben135 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let taryn900 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let tven590 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let uriel783 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let venezia719 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let yahir710 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let yasmine743 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+let yuki001 = {
+    "photo1": true,
+    "photo2": false,
+    "photo3": false,
+    "photo4": false,
+    "web1": false,
+    "web2": false,
+    "animation": false,
+    "design": false
+};
+
+
+var rand = Math.floor(Math.random() * anchor.length);
+
+document.getElementById("link").innerHTML = anchor[rand];
+
+
+
+
+
+function courseDisplay(student) {
+    if (student.photo1) {
+        document.getElementById("photo1").classList.remove("hide");
+    }
+}
